347a182b9c956b56d69688aaaad6efdc917ee3d4
OBouch21 <oliverbouchier@proton.me>
2024-01-11 13:55:18 +0000

    Set up the default board using the piecetypes enum, can use this board and the for loops in the board init to fill the board with pieces using their classes

--- Program.cs
+++ Program.cs
@@ -1,32 +1,37 @@
 ﻿// Make a chess engine
 using System.Runtime.InteropServices;
+using boardClass;
 using pieceClasses;
 
 class main{
     public static int Main(){
 
-        Pawn pawn1 = new Pawn(pieceClasses.PieceType.Pawn, true);
-        KingRook king1 = new KingRook(pieceClasses.PieceType.King, false);
+        // Pawn pawn1 = new Pawn(pieceClasses.PieceType.Pawn, true);
+        // KingRook king1 = new KingRook(pieceClasses.PieceType.King, false);
 
-        string txt1 = "";
-        string txt2 = "";
-        Piece pieceToAnalyse;
+        // string txt1 = "";
+        // string txt2 = "";
+        // Piece pieceToAnalyse;
 
-        pieceToAnalyse = king1;
+        // pieceToAnalyse = king1;
 
-        if(pieceToAnalyse.IsTaken() == false){
-            txt1 = "not ";
-        }
-        if(pieceToAnalyse.IsWhite() == true){
-            txt2 = "white";
-        } else {
-            txt2 = "black";
-        }
+        // if(pieceToAnalyse.IsTaken() == false){
+        //     txt1 = "not ";
+        // }
+        // if(pieceToAnalyse.IsWhite() == true){
+        //     txt2 = "white";
+        // } else {
+        //     txt2 = "black";
+        // }
 
-        pieceToAnalyse = king1;
+        // pieceToAnalyse = king1;
 
 
-        Console.WriteLine($"This is a {pieceToAnalyse.GetPieceType()}. It has {txt1}been taken. It's colour is {txt2}!" );
+        // Console.WriteLine($"This is a {pieceToAnalyse.GetPieceType()}. It has {txt1}been taken. It's colour is {txt2}!" );
+
+        // return 0;
+
+        Board mainbaord = new Board();
 
         return 0;
     }
--- classes.cs
+++ classes.cs
@@ -1,71 +1,141 @@
-namespace pieceClasses{
+using pieceClasses;
 
-    public enum PieceType{
+namespace pieceClasses
+{
+
+    public enum PieceType
+    {
         Pawn,
         Rook,
         Knight,
         Bishop,
         Queen,
-        King
+        King,
+        Null
     };
 
-    class Piece {
+    class Piece
+    {
         private bool isWhite;
         private bool isTaken;
         private PieceType type;
 
-        public Piece(PieceType type, bool isWhite){
+        public Piece(PieceType type, bool isWhite)
+        {
             this.isWhite = isWhite;
             this.isTaken = false;
             this.type = type;
         }
-        public bool IsWhite(){
+        public bool IsWhite()
+        {
             return isWhite;
         }
 
-        public bool IsTaken(){
+        public bool IsTaken()
+        {
             return isTaken;
         }
 
-        public PieceType GetPieceType(){
+        public PieceType GetPieceType()
+        {
             return type;
         }
 
-        public void takePiece(){
+        public void takePiece()
+        {
             isTaken = true;
         }
     };
 
-    class KingRook : Piece { // Both king and rook have normal moves besides castling
+    class KingRook : Piece
+    { // Both king and rook have normal moves besides castling
 
         private bool canCastle;
-        public KingRook(PieceType pieceType, bool isWhite):base(pieceType, isWhite){
+        public KingRook(PieceType pieceType, bool isWhite) : base(pieceType, isWhite)
+        {
 
-            this.canCastle = true;    
+            this.canCastle = true;
         }
 
-        public bool CanCastle(){
+        public bool CanCastle()
+        {
             return canCastle;
         }
 
-        public void setCanCastle(bool canCastle){
+        public void setCanCastle(bool canCastle)
+        {
             this.canCastle = canCastle;
         }
     };
 
-    class Pawn : Piece { // TODO: Add En Passant
+    class Pawn : Piece
+    { // TODO: Add En Passant
         public bool hasMoved;
-        public Pawn(PieceType pieceType, bool isWhite):base(pieceType, isWhite){
+        public Pawn(PieceType pieceType, bool isWhite) : base(pieceType, isWhite)
+        {
             this.hasMoved = false;
         }
 
-        public bool HasMoved(){
+        public bool HasMoved()
+        {
             return hasMoved;
         }
 
-        public void setHasMoved(bool hasMoved){
+        public void setHasMoved(bool hasMoved)
+        {
             this.hasMoved = hasMoved;
         }
     };
+
+}
+
+namespace boardClass
+{
+    class Board
+    {
+        private Piece[,] boardState;
+        private Piece rook1;
+        private PieceType[,] defaultBoard = {{PieceType.Rook,PieceType.Knight,PieceType.Bishop,PieceType.King,PieceType.Queen,PieceType.Bishop,PieceType.Knight,PieceType.Rook},
+                                                {PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn},
+                                                {PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null},
+                                                {PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null},
+                                                {PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null},
+                                                {PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null,PieceType.Null},
+                                                {PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn,PieceType.Pawn},
+                                                {PieceType.Rook,PieceType.Knight,PieceType.Bishop,PieceType.Queen,PieceType.King,PieceType.Bishop,PieceType.Knight,PieceType.Rook}};
+
+        public Board()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j =0; j < 8; j++){
+                    switch (defaultBoard[i,j]){
+                        case PieceType.Rook:
+                            Console.WriteLine("ROOK");
+                            break;
+                        case PieceType.Pawn:
+                            Console.WriteLine("PAWN");
+                            break;
+                        case PieceType.Knight:
+                            Console.WriteLine("KNIGHT");
+                            break;
+                        case PieceType.Bishop:
+                            Console.WriteLine("BISHOP");
+                            break;
+                        case PieceType.King:
+                            Console.WriteLine("KING");
+                            break;
+                        case PieceType.Queen:
+                            Console.WriteLine("QUEEN");
+                            break;
+                        default:
+                            Console.WriteLine("NO PIECE");
+                            break;
+                            
+                        }
+                    }
+            }
+        }
+    }
 
 }
